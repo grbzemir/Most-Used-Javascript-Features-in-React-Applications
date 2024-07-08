56e29c1d7b77a8cf8a00cabd1e91b4638cc43a72
EMİRCAN YETKİN GÜRBÜZ <eyetkingurbuz@ogr.trakya.edu.tr>
2024-07-08 19:04:54 +0300

    ArrowFunction

---
+++ arrowFunction.js
@@ -0,0 +1,12 @@
+//Arrow Function(oklu fonksiyon)
+
+// console.log(topla(5, 10));
+
+// function topla(sayi1, sayi2) {
+//     return sayi1 + sayi2;
+// }
+
+
+const topla = (sayi1, sayi2) => sayi1 + sayi2;
+
+console.log(topla(5, 10));
