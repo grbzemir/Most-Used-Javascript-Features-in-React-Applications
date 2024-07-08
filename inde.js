e56aca7b44b6d9807fc94ef936a05e39b4a7937b
EMİRCAN YETKİN GÜRBÜZ <eyetkingurbuz@ogr.trakya.edu.tr>
2024-07-08 18:54:13 +0300

    first commit

---
+++ inde.js
@@ -0,0 +1,19 @@
+//Template literals(Şablon dizileri)
+
+
+const isim = "Mehmet";
+
+const mesaj = `Merhaba ${isim}`;
+
+console.log(mesaj);
+
+
+const birim_fiyat = 1000;
+
+const adet = 3;
+
+const para_birimi = 'TL';
+
+const Toplam_Tutar = `Toplam: ${birim_fiyat * adet} ${para_birimi}`;
+
+console.log(Toplam_Tutar);
